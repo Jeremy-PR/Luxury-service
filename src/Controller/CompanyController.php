d58a9ea1a4dd1a12db70090ad0439a5ddbff6973
Jeremy-PR <jeremy.pelisse@gmail.com>
2025-02-06 16:27:15 +0100

    second day

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -6,15 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController{
-    #[Route('/', name: 'app_home')]
+final class CompanyController extends AbstractController
+{
+    #[Route('/company', name: 'app_company')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-        
+        return $this->render('company/index.html.twig', [
+            'controller_name' => 'CompanyController',
         ]);
     }
-
-
-
 }
